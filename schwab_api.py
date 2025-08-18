6a40118a89b0ea3ea7c4f688e7b1c5fbaf30de26
Tony Chung <chungtony83@gmail.com>
2025-08-18 17:48:06 +0800

    modified schwab_api.py

--- schwab_api.py
+++ schwab_api.py
@@ -8,8 +8,8 @@ import sqlalchemy as sa
 from typing import List
 
 class SchwabApi:
-    def __init__(self, ):
-        self.access_token = SchwabAuth().get_token()
+    def __init__(self, access_token = SchwabAuth().get_token(), account_number:int = 0):
+        self.access_token = access_token
         self.account_number_hash = '' ## Got this from database
         self.server_mapping = {
         'trader' : 'https://api.schwabapi.com/trader/v1',
@@ -25,22 +25,35 @@ class SchwabApi:
         
         self.default_params = {
             'accounts': {'fields': 'positions'},
-            'orders': {''}
+            'orders': {'fromEnteredTime': (datetime.datetime.now() - datetime.timedelta(days=1)).isoformat(),
+                       'toEnteredTime': datetime.datetime.now().isoformat()},
         }
 
-    def get_api_data(self, endpoint:str, params:dict = {}) -> dict:
-        if endpoint not in self.url_keys:
-            logger.error(f"Invalid endpoint: {endpoint}. Available endpoints: {list(self.url_keys)}")
-            raise ValueError(f"Invalid endpoint: {endpoint}. Available endpoints: {list(self.url_keys)}")
-        
-        url = self.url_mapping[endpoint]
+    def get_api_data(self, endpoint:str='', params:dict = {}, url:str='') -> dict:
+        if endpoint not in self.url_keys and url == '':
+            logger.error(f"Invalid endpoint: {endpoint}. Available endpoints: {', '.join(self.url_keys)}")
+            return {"error": "Invalid endpoint", "status_code": 400, "message": "Endpoint not found"}
+        elif endpoint not in self.url_keys and url != '':
+            logger.info(f"Using custom URL: {url}")
+            url = url
+        else:
+            url = self.url_mapping[endpoint]
+            
         headers = {"Authorization": f"Bearer {self.access_token}"}
+        if params == {} and endpoint in self.default_params:
+            params = self.default_params[endpoint]
+            
         r = requests.get(url, headers=headers, params=params)
-        r.raise_for_status()
-        return r.json()
+        try:
+            r.raise_for_status()
+            return r.json()
+        except requests.exceptions.HTTPError as e:
+            logger.error(f"HTTP error occurred: {e.response.status_code} - {e.response.text}")
+            return {"error": str(e), "status_code": e.response.status_code, "message": e.response.text}
+        
 
         
 if __name__ == '__main__':
     api = SchwabApi()
-    data = api.get_api_data('accounts', params = {'fields':'positions'})
+    data = api.get_api_data('accounts')
     print(data)
